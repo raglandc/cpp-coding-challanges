4b6de8219ccf4d31950f0012ee2430841779bf2b
Chris Ragland <Chrisragland97@gmail.com>
2021-11-02 11:58:18 -0400

    challange-nine

---
+++ challange-nine.cpp
@@ -0,0 +1,95 @@
+/*
+CHALLENGE CODE WRITTEN BY CHRIS RAGLAND
+COPYRIGHT 2021
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    //selection
+    char selection;
+    //list
+    vector<int> list ;
+
+    do{
+    //display menu to user
+    cout << "Menu" << endl;
+    cout << "Enter 'P' or 'p' to see the values of the list." << endl;
+    cout << "Enter 'A' or 'a' to add a number to the list." << endl;
+    cout << "Enter 'M' or 'm' to find the mean of the list." << endl;
+    cout << "Enter 'L' or 'l' to find the largest number of the list." << endl;
+    cout << "Enter 'S' or 's' to find the smallest number of the list." << endl;
+    cout << "Enter 'Q' or 'q' to quite the program." << endl;
+    cin >> selection;
+
+    //if statement to handle all cases
+    
+    if(selection == 'Q' || selection == 'q') {
+        cout << "Goodbye\n" << endl;
+        break;
+    } else if (selection == 'P' || selection == 'p') {
+        if(list.empty()) {
+            cout << "[ ]" << endl;
+        } else {
+            cout << "[ " ;
+            for(int i = 0; i < list.size(); i++) {
+                cout << list[i] << " ";
+            }
+            cout << "]";
+        }
+    } else if(selection == 'A' || selection == 'a'){
+
+            int number = 0;
+            cout << "Please enter a number: ";  
+            cin >> number;
+            list.push_back(number);
+            cout << number << " added" << endl;
+    } else if(selection == 'M' || selection == 'm') {
+            if(list.empty()) {
+                cout << "Your list is empty, no mean value to find." << endl;
+            } else {
+                int mean = 0;
+                int total = 0;
+                for(int i=0; i < list.size(); i++) {
+                    total += list[i];
+                }
+                mean = total / 2;
+                cout << "Your mean value is " << mean << endl;
+            }
+    } else if (selection == 'S' || selection == 's') {
+        if(list.empty()) {
+            cout << "List is Empty\n";
+        }else {
+            int smallest = list[0];
+            for(int i=1; i < list.size(); i++){
+                if(list[i] < smallest){
+                    smallest = list[i];
+                }
+            }
+            cout << "The smallest number in the list is "<< smallest << endl;
+        }
+    } else if (selection == 'L' || selection == 'l') {
+        if(list.empty()) {
+            cout << "List is Empty" << endl;
+        }else {
+            int largest = list[0];
+            for(int i=1; i < list.size(); i++){
+                if(list[i] > largest){
+                    largest = list[i];
+                }
+            }
+            cout << "The smallest number in the list is "<< largest << endl;
+        }
+    } else {
+        //This is the defaulting value for invalid key entries
+        cout << "That is an invalid keypress, enter a key listed in the menu" << endl;
+    }
+    } while(selection != 'q' && selection != 'Q');
+
+    return 0;
+}
---
+++ completed_projects/helloworld/helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int a = 10;
+
+    int b = 20;
+
+    cout << ((a > b) ? "A is greater than b, false" : "B is greater than a, true");
+
+    cout << "Hello World" << endl;
+
+    return 0;
+}
